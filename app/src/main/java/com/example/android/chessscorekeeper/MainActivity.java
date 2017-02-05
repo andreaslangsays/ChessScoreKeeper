80f260e837784c3a219a0b90513b826e7ab3bab7
Andreas Lang <galaxie1212@gmail.com>
2017-02-05 12:59:45 +0100

    connected functionality to buttons representation of score still needs to be fixed

--- app/src/main/java/com/example/android/chessscorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/chessscorekeeper/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.chessscorekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+     public void winPlayerA(View v) {
+         scorePlayerA +=1;
+         displayForPlayerA(scorePlayerA);
+     }
+
+    public void winPlayerB(View v) {
+        scorePlayerB +=1;
+        displayForPlayerB(scorePlayerB);
+
+    }
+
+    public void remis(View v) {
+        scorePlayerA +=.5;
+        scorePlayerB +=.5;
+        displayForPlayerA(scorePlayerA);
+        displayForPlayerB(scorePlayerB);
+    }
+
+    public void resetScore(View v) {
+        scorePlayerA = 0;
+        scorePlayerA = 0;
+        displayForPlayerA(scorePlayerA);
+        displayForPlayerB(scorePlayerA);
+
+    }
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForPlayerA(float score) {
+        TextView scoreView = (TextView) findViewById(R.id.scorePlayerA);
+        scoreView.setText(String.valueOf(score));
+    }
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForPlayerB(float score) {
+        TextView scoreView = (TextView) findViewById(R.id.scorePlayerB);
+        scoreView.setText(String.valueOf(score));
     }
 
 }
